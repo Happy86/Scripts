ff7a73e2c102e674f754771aed95b4cfe56ab14b
Andreas Boesen <andreas@boesen-art.de>
2012-11-03 20:14:24 +0100

    small is a real HTML tag, who knew.
    http://whatthecommit.com/c102af9eb2811d21176a7eb38a2e19c2
    
    	new file:   php/show-my-ip.php

---
+++ php/show-my-ip.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ * Inspiriert von:   http://checkip.dyndns.org/index.html
+ * Ausgabeformat:    <html><head><title>Current IP Check</title></head><body>Current IP Address: 10.x.y.z</body></html>
+ * Grund:            Fuer die Kuschelbude gedacht. :P
+ * Lizenzbla/Autor:  Ich stelle dieses PHP Skript unter die WTFPL
+ *                   Bei problemen koennt ihr euch auf Google melden.
+ *                   (WTFPL) 2012 Happy
+ */
+
+/* This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://sam.zoy.org/wtfpl/COPYING for more details. */
+
+
+
+
+// Init vars with NULL pointer.
+$userIP = null;
+$outStream = null;
+
+// Finding out your IP Address.
+if (!isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+   $userIP = $_SERVER['REMOTE_ADDR'];
+} else {
+   $userIP = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}
+
+// IP Address could not be found. Use mana potion. ... 0.o
+if ($userIP == null) {
+   die("YOU FAIL!!!");
+} else {
+   $outStream = "<html><head><title>Current IP Check</title></head><body>Current IP Address: ".$userIP."</body></html>";
+}
+
+// Make sure nobody has injected something strange! If so -> die!!!
+if(!filter_var($userIP, FILTER_VALIDATE_IP)){
+   die("DIE SCUM!!!");
+}
+
+// Print out result. YEAH ... EPIC SUCCESS!!!
+echo $outStream;
+
+?>
