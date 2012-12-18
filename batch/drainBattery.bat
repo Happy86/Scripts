db6979fb2ed3d2d498d7c469932d98a098f30352
Andreas Boesen <andreas@boesen-art.de>
2012-12-18 18:00:22 +0100

    change :-)
    	modified:   drainBattery.bat

--- batch/drainBattery.bat
+++ batch/drainBattery.bat
@@ -5,8 +5,8 @@ REM            wanted to drain a notebook battery using Windows NT 6.1.
 REM @licence:  "THE BEER-WARE LICENSE" (Revision 42):
 REM            As long as you retain this notice you can do whatever 
 REM            you want with this stuff. If we meet some day, and you think
-REM            this stuff is worth it, you can buy me (Johannes) a beer 
-REM            in return.
+REM            this stuff is worth it, you can buy me (Johannes) or Selfnet
+REM            ClubMate or a beer in return.
 REM 
 
 
