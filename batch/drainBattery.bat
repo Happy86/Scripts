1e83ade7decd713271b73c0d6cc09f15561c132b
Andreas Boesen <andreas@boesen-art.de>
2012-12-18 17:32:22 +0100

    added header comment with licence and stuff.
    	modified:   drainBattery.bat

--- batch/drainBattery.bat
+++ batch/drainBattery.bat
@@ -1,3 +1,15 @@
+REM 
+REM @author:   (c) >/< 2003 by Johannes (at Selfnet.de)
+REM @about:    This script was implemented by Johannes for Andreas who 
+REM            wanted to drain a notebook battery using Windows NT 6.1.
+REM @licence:  "THE BEER-WARE LICENSE" (Revision 42):
+REM            As long as you retain this notice you can do whatever 
+REM            you want with this stuff. If we meet some day, and you think
+REM            this stuff is worth it, you can buy me (Johannes) a beer 
+REM            in return.
+REM 
+
+
 @echo off
 
 REM color z.B. 0A oder 0D
