8fe14f7bd464440c5a21614c1628c185a96bb6e1
Andreas Boesen <andreasb@selfnet.de>
2013-05-06 18:43:58 +0200

    make bash scritp to get quota via xml source more awesome :P

--- bash/selfnet-quota-xml.sh
+++ bash/selfnet-quota-xml.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This file is a bash script called "selfnet-quota-xml.sh".
-# Copyright (C) 2012 Andreas Boesen
+# Copyright (C) 2013 Andreas Boesen
 # 
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
@@ -23,11 +23,10 @@
 ## Abstract:    Shows the current quota.  
 ##              Tested with Debian Testing. 
 ## Date:        2012-12-12
-## Version:     0.1
+## Version:     0.2
 ## Licence:     GPLv2
 
 #http://www.selfnet.de/quota.xml
-
 
 query=$(curl --silent http://www.selfnet.de/quota.xml);
 lineCountOfQuery=$(echo "$query" | wc -l);
@@ -71,9 +70,42 @@ availableInPercent=$( echo "$(echo "$available" | cut -b 15- | cut -d ' ' -f 4 |
 
 
 # output
-echo -e "	You have used \033[1m$usedInGiBs""GiB\033[0m of \033[1m$totalInGiBs""GiB. 						\033[1m($usedInGiBs""GiB / $totalInGiBs""GiB)\033[0m	($usedInBytes bytes / $totalInBytes bytes)";
-echo -e "	This means that you used \033[1m$usedInPercent""%\033[0m and you have \033[1m$availableInPercent""%\033[0m left of your total Quota.		\033[1m($usedInPercent""% / $availableInPercent""%)\033[0m";
-echo -e "	You can still produce \033[1m$availableInGiBs""GiB\033[0m of traffic in this month.";
+#echo -e "	You have used \033[1m$usedInGiBs""GiB\033[0m of \033[1m$totalInGiBs""GiB. 						\033[1m($usedInGiBs""GiB / $totalInGiBs""GiB)\033[0m	($usedInBytes bytes / $totalInBytes bytes)";
+#echo -e "	This means that you used \033[1m$usedInPercent""%\033[0m and you have \033[1m$availableInPercent""%\033[0m left of your total Quota.		\033[1m($usedInPercent""% / $availableInPercent""%)\033[0m";
+#echo -e "	You can still produce \033[1m$availableInGiBs""GiB\033[0m of traffic in this month.";
+
+if [ -z "$1" ]; then 
+   # default (used GiB / total GiB)
+   echo $usedInGiBs " GiB / " $totalInGiBs" GiB";
+elif [ "$1" == "-v" ] || [ "$1" == "--verbose" ]; then
+   # -v or --help 
+   echo -e "	You have used \033[1m$usedInGiBs""GiB\033[0m of \033[1m$totalInGiBs""GiB.
+	\033[1m($usedInGiBs""GiB / $totalInGiBs""GiB)\033[0m	($usedInBytes bytes / $totalInBytes bytes)
+				";
+   echo -e "	This means that you used \033[1m$usedInPercent""%\033[0m and you have \033[1m$availableInPercent""%\033[0m left of your total Quota.
+	\033[1m($usedInPercent""% / $availableInPercent""%)\033[0m
+				";
+   echo -e "	You can still produce \033[1m$availableInGiBs""GiB\033[0m of traffic in this month.";
+else
+   # give help if $1 is set
+   echo "selfnet-quota-xml Bash Script (c)2013 by Andreas Boesen";
+	echo "Licence: GNU GPL Version 2 - http://www.gnu.org/licenses/gpl-2.0.html";
+	echo "";
+	echo "usage: quota [-o|--option]";
+	echo "-v --verbose			DE: Viele unnuetze Informationen.";
+	echo "				EN: A lot of useless information.";
+   echo "";
+   echo "-h --help			DE: Diesen Text anzeigen.";
+	echo "				EN: Display the text you are reading right now.";
+	echo "";
+fi
+
+
+
+
+
+
+
 
 
 
